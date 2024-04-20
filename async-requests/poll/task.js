e5ac601de9ed753aef3636321452c6aaa1c86a5f
Tatiana Afonin <tatiana-afonina@gmail.com>
2024-04-20 22:37:50 +0200

    pool home work

---
+++ async-requests/poll/task.js
@@ -0,0 +1,37 @@
+const pollTitleElement = document.getElementById('poll__title');
+const pollAnswersElement = document.getElementById('poll__answers');
+
+const refreshQuestions = () => {
+  let xhr = new XMLHttpRequest();
+  
+  xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.addEventListener('readystatechange', () => {
+    if(xhr.readyState === XMLHttpRequest.DONE){
+      pollAnswersElement.innerHTML = '';
+
+      const { data } = JSON.parse(xhr.responseText);
+  
+      console.log(data);
+      
+      pollTitleElement.textContent = data.answers;
+  
+      for (let key in data.answers) {
+        const buttonElement = document.createElement('button');
+  
+        buttonElement.className = 'poll__answer';
+        buttonElement.textContent = data.answers[key];
+        pollAnswersElement.appendChild(buttonElement);
+  
+        buttonElement.onclick = () => {
+          window.alert('Спасибо, ваш голос засчитан!');
+          refreshQuestions();
+        }
+      }
+    }
+  });
+   
+  xhr.send();
+}
+
+refreshQuestions();
