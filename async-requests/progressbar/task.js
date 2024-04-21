3903a89a86727981bd8d848b637db5b953934f0f
Tatiana Afonin <tatiana-afonina@gmail.com>
2024-04-21 17:53:46 +0200

    progressBar

---
+++ async-requests/progressbar/task.js
@@ -0,0 +1,41 @@
+const progressElement = document.getElementById('progress');
+const formElement = document.getElementById('form');
+const sendButtonElement = document.getElementById('send');
+
+//progress.value = 0.7;
+
+const sendFiles = () => {
+  let xhr = new XMLHttpRequest();
+  //debugger
+  const formData = new FormData(formElement);
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
+
+  xhr.onprogress = (event) => {
+    progressElement.value = event.loaded;
+    progressElement.max = event.total;
+  };
+
+  xhr.addEventListener('readystatechange', () => {
+     const data = JSON.parse(xhr.responseText);
+  
+     console.log(data);
+  });
+
+  xhr.upload.onload = function() {
+    alert( 'Данные полностью загружены на сервер!' );
+  }
+
+  xhr.upload.onerror = function() {
+    progressElement.value = 0;
+    
+    alert( 'Произошла ошибка при загрузке данных на сервер!' );
+  }
+   
+  xhr.send(formData);
+}
+
+formElement.onsubmit = (event) => {
+  event.preventDefault();
+
+  sendFiles();
+}
